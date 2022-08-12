dd932d0bd8188c2ae71d2cf5978e425c20cfff61
Elisabete Martins de Oliveira <bete.mrt@gmail.com>
2022-08-12 18:01:57 -0300

    ajustando alguns elementos do Lint

--- script.js
+++ script.js
@@ -4,19 +4,7 @@ const btnlogin = document.querySelector('#btnlogin');
 const email = document.querySelector('#emailLogin');
 const senha = document.querySelector('#senha');
 
-btnlogin.addEventListener('click', function(event) {
-  event.preventDefault();
-  const validacao = validacaoEmail();
-  if (validacao === false) {
-    alert('Email ou senha inválidos.');
-  } else {
-    alert('Olá, Tryber!');
-  }
-  email.value = "";
-  senha.value = "";
-});
-
-function validacaoEmail() {
+function validacaoEmail () {
   const emailInvalido = email.value.length < 10 || email.value.length > 50;
   const senhaInvalida = senha.value.length < 6 || senha.value.length > 20;
   if (emailInvalido || senhaInvalida) {
@@ -27,21 +15,33 @@ function validacaoEmail() {
   }
 }
 
+btnlogin.addEventListener ('click', (event) => {
+  event.preventDefault();
+  const validacao = validacaoEmail();
+  if (validacao === false) {
+    alert('Email ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
+  }
+  email.value = '';
+  senha.value = '';
+});
+
 // inserindo os radios de avalização
 // id=avaliation
 const radioBox = document.querySelector('#avaliation');
 
 function radiosAvaliacao() {
-  for(let i = 0; i <= 10; i += 1) {
-    let labelOpt = document.createElement('label');
+  for (let i = 0; i <= 10; i += 1) {
+    const labelOpt = document.createElement('label');
     labelOpt.innerHTML = i;
     labelOpt.htmlFor = i;
-    let rateOpt = document.createElement('input');
-    rateOpt.type ="radio";
-    rateOpt.name = "rate";
+    const rateOpt = document.createElement('input');
+    rateOpt.type = 'radio';
+    rateOpt.name = 'rate';
     rateOpt.value = i;
     rateOpt.id = i;
-    rateOpt.className = "rate";
+    rateOpt.className = 'rate';
     radioBox.appendChild(rateOpt);
     radioBox.appendChild(labelOpt);
   }
